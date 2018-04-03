a4b12f7b3b3431d86c3492e64a7d268e304b9068
ndm13 <ndm0113@gmail.com>
2018-04-03 17:04:05 -0400

    Initial commit

---
+++ htmlversion/includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <span id="pct"><a href="https://www.pct.edu/" target="_blank">Penn College</a></span>
+    <span id="app">Association of Professional Programmers</span>
+    <nav><ul>
+        <a href="../../pico/index.php"><li>Home</li></a>
+        <a href="events.html"><li>Events</li></a>
+        <a href="about.html"><li>About</li></a>
+        <a href="resources.html"><li>Resources</li></a>
+    </ul></nav>
+</header>
